9f1ff9595b35efc3c9374cb5218941440dfdfd4b
jyuch <jyuch@users.noreply.github.com>
2015-08-26 21:18:12 +0900

    基本の実装

---
+++ src/ReflectionToStringBuilder.Tests/ToStringBuilderTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jyuch.ReflectionToStringBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jyuch.ReflectionToStringBuilder.Tests
+{
+    [TestClass()]
+    public class ToStringBuilderTests
+    {
+        [TestMethod()]
+        public void ProcessSinglePropertyClass()
+        {
+            var property1Value = "Property1";
+            var source = new SinglePropertyClass() { Property1 = property1Value };
+            var expected = $"{nameof(SinglePropertyClass)}{{{nameof(SinglePropertyClass.Property1)}={property1Value}}}";
+            var actual = ToStringBuilder.ToString(source);
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void ProcessDualPropertyClass()
+        {
+            var property1Value = "Property1";
+            var property2Value = "Property2";
+            var source = new DualPropertyClass() { Property1 = property1Value, Property2 = property2Value };
+            var expected = 
+                $"{nameof(DualPropertyClass)}{{" + 
+                $"{nameof(DualPropertyClass.Property1)}={property1Value}," + 
+                $"{nameof(DualPropertyClass.Property2)}={property2Value}}}";
+            var actual = ToStringBuilder.ToString(source);
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void ProcessTriplePropertyClass()
+        {
+            var property1Value = "Property1";
+            var property2Value = "Property2";
+            var property3Value = "Property3";
+            var source = new TriplePropertyClass()
+            {
+                Property1 = property1Value,
+                Property2 = property2Value,
+                Property3 = property3Value
+            };
+            var expected =
+                $"{nameof(TriplePropertyClass)}{{" +
+                $"{nameof(TriplePropertyClass.Property1)}={property1Value}," +
+                $"{nameof(TriplePropertyClass.Property2)}={property2Value}," +
+                $"{nameof(TriplePropertyClass.Property3)}={property3Value}}}";
+            var actual = ToStringBuilder.ToString(source);
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void ProcessNullProperty()
+        {
+            string property1Value = null;
+            var source = new SinglePropertyClass() { Property1 = property1Value };
+            var expected = $"{nameof(SinglePropertyClass)}{{{nameof(SinglePropertyClass.Property1)}={string.Empty}}}";
+            var actual = ToStringBuilder.ToString(source);
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(actual, expected);
+        }
+
+        private class SinglePropertyClass
+        {
+            public string Property1 { get; set; }
+        }
+
+        private class DualPropertyClass
+        {
+            public string Property1 { get; set; }
+            public string Property2 { get; set; }
+        }
+
+        private class TriplePropertyClass
+        {
+            public string Property1 { get; set; }
+            public string Property2 { get; set; }
+            public string Property3 { get; set; }
+        }
+    }
+}
---
+++ src/ReflectionToStringBuilder/ToStringBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Jyuch.ReflectionToStringBuilder
+{
+    public static class ToStringBuilder
+    {
+        private static ConcurrentDictionary<Type, IEnumerable<PropertyAccessor>> _cache
+            = new ConcurrentDictionary<Type, IEnumerable<PropertyAccessor>>();
+
+        private class PropertyAccessor
+        {
+            public PropertyInfo PropertyInfo { get; }
+            public Func<object, object> Accessor { get; }
+
+            public PropertyAccessor(PropertyInfo info, Func<object, object> accessor)
+            {
+                PropertyInfo = info;
+                Accessor = accessor;
+            }
+        }
+
+        public static string ToString<T>(T obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+            var objType = typeof(T);
+            IEnumerable<PropertyAccessor> exprs;
+            if (!_cache.TryGetValue(objType, out exprs))
+            {
+                exprs = InitAccessor(objType);
+                _cache.TryAdd(objType, exprs);
+            }
+
+            var toStringText = new StringBuilder();
+            toStringText.Append(objType.Name).Append("{");
+
+            if (exprs.Count() != 0)
+            {
+                var a = exprs.First();
+                toStringText.Append(PropertyFormatter(a, obj));
+
+                foreach (var it in exprs.Skip(1))
+                {
+                    toStringText.Append(",");
+                    toStringText.Append(PropertyFormatter(it, obj));
+                }
+            }
+
+            toStringText.Append("}");
+            return toStringText.ToString();
+        }
+
+        private static IEnumerable<PropertyAccessor> InitAccessor(Type targetType)
+        {
+            var toStringProp = targetType.GetProperties()
+                .Where((it) => it.CanRead)
+                .Where((it) => it.GetIndexParameters().Length == 0);
+
+            var result = new List<PropertyAccessor>();
+
+            foreach (var it in toStringProp)
+            {
+                // same as it => ((targetType)it).Property
+                var arg = Expression.Parameter(typeof(object), "it");
+                var convToTarget = Expression.Convert(arg, targetType);
+                var getPropValue = Expression.MakeMemberAccess(convToTarget, it);
+                var convToObject = Expression.Convert(getPropValue, typeof(object));
+                var lambda = Expression.Lambda(convToObject, arg);
+                Func<object, object> expr = (Func<object, object>)lambda.Compile();
+                result.Add(new PropertyAccessor(it, expr));
+            }
+
+            return result;
+        }
+
+        private static string PropertyFormatter(PropertyAccessor p, object obj)
+        {
+            return $"{p.PropertyInfo.Name}={p.Accessor(obj)?.ToString() ?? string.Empty}";
+        }
+    }
+}
