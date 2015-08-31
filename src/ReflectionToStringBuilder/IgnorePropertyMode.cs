8ad3e5267232bad94a7df5dd5905963be71cdd79
jyuch <jyuch@users.noreply.github.com>
2015-08-31 22:39:24 +0900

    XMLドキュメントコメントの整備

--- src/ReflectionToStringBuilder/IgnorePropertyMode.cs
+++ src/ReflectionToStringBuilder/IgnorePropertyMode.cs
@@ -1,9 +1,23 @@
 ﻿namespace Jyuch.ReflectionToStringBuilder
 {
+    /// <summary>
+    /// プロパティの<c>null</c>もしくは空白で無視するモードを列挙します。
+    /// </summary>
     public enum IgnorePropertyMode
     {
+        /// <summary>
+        /// <c>null</c>もしくは空白にかかわらずすべてのプロパティを文字列形式に含めます。
+        /// </summary>
         None,
+        
+        /// <summary>
+        /// プロパティが<c>null</c>の場合は文字列形式に含めません。
+        /// </summary>
         Null,
+
+        /// <summary>
+        /// プロパティが<c>null</c>もしくは空白のの場合は文字列形式に含めません。
+        /// </summary>
         NullOrWhiteSpace
     }
 }
--- src/ReflectionToStringBuilder/ToStringConfig.cs
+++ src/ReflectionToStringBuilder/ToStringConfig.cs
@@ -5,10 +5,17 @@ using System.Reflection;
 
 namespace Jyuch.ReflectionToStringBuilder
 {
+    /// <summary>
+    /// オブジェクトの文字列形式を生成するときの設定を表します。
+    /// </summary>
+    /// <typeparam name="T">出力対象となるオブジェクトの型。</typeparam>
     public sealed class ToStringConfig<T>
     {
         private HashSet<PropertyInfo> _ignoreProperty = new HashSet<PropertyInfo>();
 
+        /// <summary>
+        /// プロパティを無視するモードを指定します。既定値は<see cref="IgnoreProperty.None"/>です。
+        /// </summary>
         public IgnorePropertyMode IgnoreMode { get; set; } = IgnorePropertyMode.None;
 
         internal HashSet<PropertyInfo> IgnoreProperty
@@ -19,6 +26,10 @@ namespace Jyuch.ReflectionToStringBuilder
             }
         }
 
+        /// <summary>
+        /// オブジェクトから文字列形式を生成するときに無視するプロパティを指定します。
+        /// </summary>
+        /// <param name="expression">無視するプロパティ。</param>
         public void SetIgnoreProperty(Expression<Func<T, object>> expression)
         {
             _ignoreProperty.Add(GetProperty(expression));
