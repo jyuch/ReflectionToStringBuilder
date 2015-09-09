4b656ed2c69d7724781fb4a59f8c5714896e1e6f
jyuch <jyuch@users.noreply.github.com>
2015-09-09 21:46:06 +0900

    コメントドキュメントを追加

--- src/ReflectionToStringBuilder/TargetType.cs
+++ src/ReflectionToStringBuilder/TargetType.cs
@@ -4,10 +4,24 @@
 
 namespace Jyuch.ReflectionToStringBuilder
 {
+    /// <summary>
+    /// 文字列形式のもととなるメンバーの選択モードを列挙します。
+    /// </summary>
     public enum TargetType
     {
+        /// <summary>
+        /// プロパティを文字列形式に含めます。
+        /// </summary>
         Property,
+
+        /// <summary>
+        /// フィールドを文字列形式に含めます。
+        /// </summary>
         Field,
+
+        /// <summary>
+        /// プロパティとフィールドの両方を文字列形式に含めます。
+        /// </summary>
         Both
     }
 }
