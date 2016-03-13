506d1e01ba13510f8242466e9a39b56d97ec8237
jyuch <jyuch@users.noreply.github.com>
2016-03-13 21:51:40 +0900

    Increment version.

--- src/ReflectionToStringBuilder/Properties/AssemblyInfo.cs
+++ src/ReflectionToStringBuilder/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("ReflectionでToStringするアレ")]
 [assembly: AssemblyCompany("jyuch")]
 [assembly: AssemblyProduct("ReflectionToStringBuilder")]
-[assembly: AssemblyCopyright("Copyright (c) 2015 jyuch")]
+[assembly: AssemblyCopyright("Copyright (c) 2015-2016 jyuch")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("7e1cb969-5d63-4c0f-89ec-a1d0604eba75")]
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
